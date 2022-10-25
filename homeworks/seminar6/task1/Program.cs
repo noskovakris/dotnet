493447c340f9fa55e99798380769d9588e0ebb51
Kristina Noskova <knoskova001@cee.ema.ad.pwcinternal.com>
2022-10-25 20:36:03 +0300

    Дамашка 6 семинар

---
+++ homeworks/seminar6/task1/Program.cs
@@ -0,0 +1,37 @@
+﻿// Задача 41: Пользователь вводит с клавиатуры M чисел. 
+// Посчитайте, сколько чисел больше 0 ввёл пользователь.
+
+// 0, 7, 8, -2, -2 -> 2
+
+// 1, -7, 567, 89, 223-> 3
+
+void userDigits (int size)
+{
+    int [] userDigits= new int [size];
+    Console.WriteLine("Введите числа: ");
+    for(int i=0; i < size; i++)
+    {
+        Console.Write($"Введите число {i+1}>>> ");
+        userDigits[i] = Convert.ToInt32(Console.ReadLine());
+    }
+    greaterZero (userDigits);
+}
+
+void greaterZero (int [] userDigits)
+{
+    int greaterZeroResult = 0;
+    for(int i=0; i < userDigits.Length; i++)
+    {
+        
+        if ((userDigits[i])>0)
+        {
+            greaterZeroResult +=1; 
+        }
+    }
+    Console.WriteLine($"Больше нуля> >>> {greaterZeroResult}");
+}
+
+
+Console.Write("Введите число элементов массива  >>> ");
+int size = Convert.ToInt16(Console.ReadLine());
+userDigits (size);
---
+++ homeworks/seminar6/task2/Program.cs
@@ -0,0 +1,30 @@
+﻿// Задача 43: Напишите программу, которая найдёт точку пересечения двух прямых, 
+// заданных уравнениями y = k1 * x + b1, y = k2 * x + b2; значения b1, k1, b2 и k2 задаются пользователем.
+
+// b1 = 2, k1 = 5, b2 = 4, k2 = 9 -> (-0,5; -0,5)
+
+void userDifindCoordinates (decimal k1, decimal k2, decimal b1, decimal b2)
+{
+    decimal fingX = 0;
+    decimal fingY = 0;
+    fingX =  Math.Round((b2 - b1)/(k1 - k2),1);
+    fingY =  Math.Round((k1 * fingX + b1),1);
+
+    Console.WriteLine($"({fingX} ; {fingY})");
+}
+
+
+Console.Write("Введите b1  >>> ");
+decimal b1 = Convert.ToDecimal(Console.ReadLine());
+
+Console.Write("Введите k1  >>> ");
+decimal k1 = Convert.ToDecimal(Console.ReadLine());
+
+
+Console.Write("Введите b2  >>> ");
+decimal b2 = Convert.ToDecimal(Console.ReadLine());
+
+
+Console.Write("Введите k2  >>> ");
+decimal k2 = Convert.ToDecimal(Console.ReadLine());
+userDifindCoordinates (k1,k2,b1,b2);
